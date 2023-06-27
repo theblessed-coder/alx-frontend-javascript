4fafe4875c34992832687cf6345f8f12727c936c
theblessedcoder <blessingnwakwuo8@gmail.com>
2023-06-27 02:16:20 +0000

    the iterateThroughObject function with a report iterator object, returns every employee name in a string separated by |:

--- 0x00-ES6_basic/101-main.js
+++ 0x00-ES6_basic/101-main.js
@@ -1,7 +1,8 @@
-import createIteratorObject from "./100-createIteratorObject.js";
-
-import createEmployeesObject from './11-createEmployeesObject.js';
+import createEmployeesObject from "./11-createEmployeesObject.js";
 import createReportObject from './12-createReportObject.js';
+import createIteratorObject from './100-createIteratorObject.js';
+import iterateThroughObject from './101-iterateThroughObject.js';
+
 
 const employees = {
     ...createEmployeesObject('engineering', ['Bob', 'Jane']),
@@ -9,9 +10,7 @@ const employees = {
 };
 
 const report = createReportObject(employees);
-
 const reportWithIterator = createIteratorObject(report);
 
-for (const item of reportWithIterator) {
-    console.log(item);
-}
+console.log(iterateThroughObject(reportWithIterator));
+
