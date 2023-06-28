a616410e42e19f4794b286eb8a9167deed9a677e
theblessedcoder <blessingnwakwuo8@gmail.com>
2023-06-28 02:56:38 +0000

    fixed a bug

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,22 @@
-// eslint-disable-next-line import/named
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
+  const s = signUpUser(firstName, lastName);
+  const u = uploadPhoto(fileName);
 
-  return Promise.allSettled([signUpPromise, uploadPromise])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason.message,
-    })));
+  return Promise.allSettled([s, u]).then((vals) => {
+    const resArr = [];
+    vals.forEach((val) => {
+      if (val.status === 'fulfilled') {
+        resArr.push({ status: val.status, value: val.value });
+      } else {
+        resArr.push({
+          status: val.status,
+          value: `Error: ${val.reason.message}`,
+        });
+      }
+    });
+    return resArr;
+  });
 }
--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -2,13 +2,11 @@ export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    queue.push(error.message);
+    queue.push(mathFunction());
+  } catch (err) {
+    queue.push(err.toString());
   } finally {
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
