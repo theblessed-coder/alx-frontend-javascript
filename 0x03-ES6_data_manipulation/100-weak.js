c4da1309c767fadfa926f2c294f7c305f178dbb6
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-07-05 02:39:47 +0100

    a new function named queryAPI tracked within the weakMap the number of times queryAPI is called for each endpoint

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,11 @@
+export const weakMap = new WeakMap();
+export function queryAPI(endpoint) {
+  let called = weakMap.get(endpoint) || 0;
+  called += 1;
+
+  if (called >= 5) {
+    throw new Error('Endpoint load is high');
+  } else {
+    weakMap.set(endpoint, called);
+  }
+}
