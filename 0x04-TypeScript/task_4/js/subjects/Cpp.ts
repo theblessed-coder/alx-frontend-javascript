f82a5ad9e73fa0e2fbe0e5989037d28d3d294945
theblessed-coder <blessingnwakwuo8@gmail.com>
2023-07-06 02:43:29 +0100

    created and exported a constant cpp for Cpp Subjects

---
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -0,0 +1,20 @@
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for C++';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC !== undefined) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
